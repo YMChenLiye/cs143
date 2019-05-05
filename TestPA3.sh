5e9e90ebc8ba3a04807e833d7e1b46822849a844
liyechen <ymchenliye@gmail.com>
2019-05-05 16:24:11 +0800

    add first version of PA3s write-up

--- TestPA3.sh
+++ TestPA3.sh
@@ -4,4 +4,5 @@ bash Docker/BuildDocker.sh
 
 docker run --rm -it -v $PWD/write-up:/root/workspace/write-up compilers sh -c \
 "mkdir build && cd build && make -f /usr/class/cs143/assignments/PA4/Makefile \
+&& cp ../write-up/PA3/* . \
 && perl ../grading/pa3-grading.pl"
